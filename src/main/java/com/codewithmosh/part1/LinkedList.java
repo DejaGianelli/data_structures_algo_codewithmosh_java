d760cb3a0ff56a4881475cecc6765d012c72ee9d
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2023-07-16 16:13:37 -0300

    The Ultimate Data Structures & Algorithms: Part 1
    Module: Linked Lists

---
+++ src/main/java/com/codewithmosh/part1/LinkedList.java
@@ -0,0 +1,135 @@
+package com.codewithmosh.part1;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    public static class Node {
+        private final int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+        } else {
+            //Creates variable that stores a reference to the current second node
+            var second = first.next;
+            //Clean the reference to the current second node inside first node.
+            first.next = null;
+            //The second node becomes the first by the reference in variable second
+            first = second;
+        }
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+        } else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        size--;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+
+    public void addLast(int item) {
+        var node = new Node(item);
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node;
+            last = node;
+        }
+        size++;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        var index = 0;
+        var current = first;
+        while (current != null) {
+            array[index] = current.value;
+            current = current.next;
+            index++;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        System.out.println(list.size());
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(5);
+        System.out.println(list.indexOf(20));
+        System.out.println(list.contains(40));
+        System.out.println(list.contains(10));
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
